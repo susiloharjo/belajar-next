cc3b15b729aa7355c574d9fa89f19c1b3bc1dbdd
susiloharjo <susiloharjo@gmail.com>
2023-12-06 14:57:52 +0700

    ngerapihin form

--- app/form/Form.jsx
+++ app/form/Form.jsx
@@ -35,42 +35,40 @@ export default function Form() {
 
     return (
       <div className="w-1/2 items-center bg-white p-10 rounded-lg m-5">
-      <form onSubmit={handleSubmit} className="w-full md:w-1/2">
-  <div className="flex flex-col py-5">
-    <label className="flex items-center">
-      <span className="w-20 mr-2">Name:</span>
-      <input
-        className="bg-slate-300 rounded-md p-2 flex-grow"
-        required
-        type="text"
-        onChange={(e) => setName(e.target.value)}
-        value={name}
-      />
-    </label>
+        <form onSubmit={handleSubmit} className="w-full md:w-1/2">
+          <div className="flex flex-col py-5 space-y-2">
+            <h1 className="font-bold text-lg">Form Login</h1>
+            <label className="flex items-center">
+              <span className="w-20 mr-2">Name:</span>
+              <input
+                className="border border-gray-300 rounded-md p-2 flex-grow"
+                required
+                type="text"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+              />
+            </label>
 
-    <label className="flex items-center">
-      <span className="w-20 mr-2">Email:</span>
-      <input
-        className="bg-slate-300 rounded-md p-2 flex-grow"
-        required
-        type="email"
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
-      />
-    </label>
-  </div>
+            <label className="flex items-center">
+              <span className="w-20 mr-2">Email:</span>
+              <input
+                className="border border-gray-300 rounded-md p-2 flex-grow"
+                required
+                type="email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+              />
+            </label>
+          </div>
 
-  <button
-    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full md:w-auto"
-    disabled={isLoading}
-  >
-    {isLoading && <span>Adding...</span>}
-    {!isLoading && <span>Submit</span>}
-  </button>
-</form>
-
-
-            
-            </div>
-  )
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full md:w-auto"
+            disabled={isLoading}
+          >
+            {isLoading && <span>Adding...</span>}
+            {!isLoading && <span>Submit</span>}
+          </button>
+        </form>
+      </div>
+    );
 }
